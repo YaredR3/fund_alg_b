1de8cd086df12a7732fdc4897540ea2200696b99
YaredR3 <2109130@upy.edu.mx>
2022-01-28 08:40:25 -0600

    estructura general C

---
+++ unit1/generalStrucProgramC.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Global variables;
+//Create a variable tipoDato nameVariable = anyData;
+//dataTypes in C/C++/C#/PHP
+int unEntero = 3;
+float unDecimal = 3.1416;
+char* name ="YaredRangel";
+char unCaracter = 'M';
+//////////////////////////////////
+
+
+/// La ejecucion del programa ///
+int main(){
+    printf("Hello Data B %s\n", name);
+    return 0;
+}
+/////////////////////////////////////////////
