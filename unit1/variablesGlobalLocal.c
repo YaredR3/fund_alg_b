c0dad4a8f4aff14490cc21ba40f6c90166b6998e
YaredR3 <2109130@upy.edu.mx>
2022-02-04 09:52:21 -0600

    local variables internal main

---
+++ unit1/variablesGlobalLocal.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Global variables;
+char* date = "02-02-22T10:25:00z";
+//////////////////////////////////
+
+/// La ejecucion del programa ///
+int main(){
+    date = "02-03-22T10:25:00z";
+    char* hours = "10:25:00z";
+    return 0;
+}
+/////////////////////////////////////////////
