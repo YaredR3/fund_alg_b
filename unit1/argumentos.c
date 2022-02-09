c10d08c97da9f487480b58340032a31893241080
YaredR3 <2109130@upy.edu.mx>
2022-02-09 11:09:21 -0600

    argv lesson

---
+++ unit1/argumentos.c
@@ -0,0 +1,8 @@
+#include <studio.h>
+#include "utils.h"
+
+int main(){
+
+
+    return 0;
+}
--- unit1/variablesGlobalLocal.c
+++ unit1/variablesGlobalLocal.c
@@ -8,7 +8,13 @@ char* date = "02-02-22T10:25:00z";
 /// La ejecucion del programa ///
 int main(){
     date = "02-03-22T10:25:00z";
-    char* hours = "10:25:00z";
+    int hours = 22; //Lomal:main  %d-%i
+    float pi = 3.1416;  //%f
+    char character = "c";  // %c
+    char* name = "Yareeeed"; //%s
+    int *address = %hours; //%p
+    hours = hours +2;
+    printf("hours: %d, %s\n", hours, name);
     return 0;
 }
 /////////////////////////////////////////////
