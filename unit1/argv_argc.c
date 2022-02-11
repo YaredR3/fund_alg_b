77461340851d513c4b3b9d243a7df7f49166926a
YaredR3 <2109130@upy.edu.mx>
2022-02-11 08:46:27 -0600

    argv lesson

--- unit1/argv_argc.c
+++ unit1/argv_argc.c
@@ -2,7 +2,9 @@
 #include <string.h>
 
 int main(int argc, char** argv){
-    printf("Program executed: %s, number of inputs elements: %d-1", argv[0], (argc-1))
+    if (argc <= 1) return 0;
+
+    printf("Program executed: %s, number of inputs elements: %d\n", argv[0], (argc-1));
     printf("Hola %s %s %s %s, bienvenido!\n", argv[1], argv[2], argv[3], argv[4]);
     return 0;
 }
