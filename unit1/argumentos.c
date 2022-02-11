5ff875fe901c2e33c9224c0362341408416a4ed9
YaredR3 <2109130@upy.edu.mx>
2022-02-11 08:23:29 -0600

    argv lesson

--- unit1/argumentos.c
+++ unit1/argumentos.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "utils.h"
+#include <stdbool.h>
 
 int main(){
 
-
+    int isGreater = IsGreaterThan10(11);
+    printf("the number is: %d", isGreater)
+    
     return 0;
 }
--- unit1/argv_argc.c
+++ unit1/argv_argc.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 int main(int argc, char** argv){
-    printf("Program executed: %s, number of inputs elements: %d", argv[0], (argc-1));
+    printf("Program executed: %s, number of inputs elements: %d-1", argv[0], (argc-1))
     printf("Hola %s %s %s %s, bienvenido!\n", argv[1], argv[2], argv[3], argv[4]);
     return 0;
 }
