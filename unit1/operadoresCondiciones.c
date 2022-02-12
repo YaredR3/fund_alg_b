9fc277fdc8cee11e414cec9a1ccfb3ed09e19c61
YaredR3 <2109130@upy.edu.mx>
2022-02-11 18:33:02 -0600

    conditions

---
+++ unit1/operadoresCondiciones.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+//temperatura con condiciones//
+// frio: 16
+// caliente: 29
+int frio = 16;
+int caliente = 29;
+int sofa = 7000
+
+int main(int argc, char** argv){
+    int inputTemp = atoi(argv[1]);
+    int inputMoney = atoi(argv[2]);
+    char inputName = argv[3];  //--> "F" {'F', 'a', 'm', 'a', 'l', 'e', '\0'};
+    bool temp_condition = (inputTemp == frio);
+    bool temp_condition2 = (inputTemp == caliente);
+    bool temp_condition3 = (inputTemp >= caliente);
+    bool canRobotMove = (inputTemp <= caliente);
+    bool hasUserMoney = (inputMoney >= sofa);
+    bool isUserMale = (inputName != 'F');
+
+    
+    if(canRobotMove){
+        // Robot.move(x_coord, y_coord);
+    } else {
+        // Robot.stop(x_coord, y_coord);
+    }
+
+    if(hasUserMoney){
+        // Allows to bus
+    } else {
+        // Rejected product
+    }
+    
+    if (inputTemp <= 29){
+        //Robot_move(x_coord, y_coord);
+    } else {
+        // Robot.stop(x_coord, y_coord);
+    }
+    
+    return 0;
+}
