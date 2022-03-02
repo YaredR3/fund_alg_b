4fb5c9a81bde0d77be89ce3f119b4a1975ef383e
YaredR3 <2109130@upy.edu.mx>
2022-03-02 11:05:54 -0600

    loops

--- unit2/loops.c
+++ unit2/loops.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "../libraries/utils.h"
+#include <stdlib.h>
 
 int main(int argc, char** argv){
 
@@ -7,5 +8,37 @@ int main(int argc, char** argv){
         printf("ITERATOR: %d\n", iterator);
         sleep(1000);  
     }
+
+    int rango = 100;
+    int count = 0;
+    while (rango > 1){
+        count = count + rango;
+        rango--; //rango = rango + 1;
+    }
+    printf("suma: %d\n", count);
+
+
+    // loops infinitos
+    while (true){
+        system("clear");
+        printf("Program runing...\n");
+        sleep(1000);
+        break;
+    }
+
+    for (;;){
+        break;
+    }
+
+    bool condicion;
+    int i = 1;
+    int cuenta = 0;
+    do {
+        cuenta += i; // cuenta = cuenta +1
+        printf("dato: %d\n", cuenta);
+        condicion = (i<3);
+        i++;
+    } while (condicion);
+    
     return 0;
 }
